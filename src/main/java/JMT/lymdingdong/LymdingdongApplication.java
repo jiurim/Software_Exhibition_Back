022afce0281f7bc187cfe8e7762fac55a6731584
urim <jiurim0316@naver.com>
2023-10-24 00:28:36 +0900

    기본세팅

---
+++ src/main/java/JMT/lymdingdong/LymdingdongApplication.java
@@ -0,0 +1,13 @@
+package JMT.lymdingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LymdingdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LymdingdongApplication.class, args);
+	}
+
+}
